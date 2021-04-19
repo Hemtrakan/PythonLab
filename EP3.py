2a46777ad1dc36d7705e796a8338419fa9ec3501
chirapon hemtrakan <chiraponkub@hotmail.com>
2021-04-19 13:43:50 +0700

    first commit

---
+++ EP3.py
@@ -0,0 +1,19 @@
+from typing import Type
+
+
+a = 1
+b = "1"
+c = 10.001
+d = True
+
+print(a)
+print(b)
+print(c)
+print(d)
+
+
+print(type(a)) # ตัวเลข
+print(type(b)) # ตัวเลขทศนิยม
+print(type(c)) #ตัวหนังสือ
+print(type(d)) # bool
+
---
+++ EP4.py
@@ -0,0 +1,11 @@
+# Data Type & Variable
+# ชื่อตัวแปร = ค่าที่เก็บในตัวแปร
+
+x = 20
+y = 3.99
+z = True
+
+print("ผลลัพธ์ = " + str(x))
+print(y)
+print(z)
+
