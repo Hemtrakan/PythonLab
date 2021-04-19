479c8b668db3147c8f740cb52b98de6ec7414979
chirapon hemtrakan <chiraponkub@hotmail.com>
2021-04-19 13:53:25 +0700

    Ep5 Ep6

---
+++ EP5.py
@@ -0,0 +1,29 @@
+#กฎการตั้งชื่อ
+
+'''
+1. ใส่ตัวเลข ตัวอักษร เครื่องหมาย
+2. ห้ามขึ้นต้นด้วยตัวเลขและสัญลักษณ์ ยกเว้น _ 
+3. ห้ามซ้ำกับ Keyword in python ได้ แก่ 
+    False class finally is return None continue for
+    lambda try True def from nonlocal while and del 
+    global not with as elif if or yield assert else
+    improt pass break except in raise 
+    ดังนี้
+4. case sensititve
+'''
+
+a = 10 
+_a = 10 
+_if = 22 #แบบนี้ได้
+''' $a = 10 แบบนี้คือไม่ได้ '''
+
+# ข้อ 4. case sensititve
+Name = "ชิรพล"
+name = "ชิรพล เหมตระการ"
+print(Name)
+print(name)
+
+
+
+
+
