491c116c728e7bf69cfdf2589c08c441bb3dd56a
chirapon hemtrakan <chiraponkub@hotmail.com>
2021-04-19 14:12:18 +0700

    Ep7

--- EP6.py
+++ EP6.py
@@ -1,2 +1,16 @@
 #Type Coversion
+x = 10
+y = 3.14
+z = "20"
 
+# บวก
+
+result = x+y
+print(result)
+_return = y+int(z)
+print(_return)
+
+a = str(x) +  " ____ " + str(y)
+print(a)
+
+print(float(z))
---
+++ EP7.py
@@ -0,0 +1,21 @@
+# function input 
+
+
+'''
+fname = input("กรุงณาป้อนชื่อของท่าน : ")
+lname = input("กรุงณาป้อนนามสกุลของท่าน : ")
+print("ชื่อของคุณคือ : " + fname +" "+lname)
+'''
+
+x = float(input("ป้อนตัวเลขตัวที่ 1 : "))
+y = float(input("ป้อนตัวเลขตัวที่ 2 : "))
+
+z = x+y
+print(z)
+
+x1 = int(input("ป้อนตัวเลขตัวที่ 1 : "))
+y1 = int(input("ป้อนตัวเลขตัวที่ 2 : "))
+
+
+z1 = x1+y1
+print(z1)
