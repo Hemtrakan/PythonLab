31a3fc2ed49311cf2d8d0574dc447a86c2b9e0a6
chirapon hemtrakan <chiraponkub@hotmail.com>
2021-04-20 16:00:31 +0700

    reName

---
+++ EP14.ControlStructure.py
@@ -0,0 +1,19 @@
+'''
+โครงสร้างควบคุม (Control Structure)
+1. แบบลำดับ
+2. แบบเลือก
+3. แบบทำซ้ำ
+'''
+
+'''
+if expression: => True OR False
+    statement
+'''
+
+age = int(input("ป้อนอายุของคุณ : "))
+name = 'ชิรพล เหมตระการ'
+if age >= 15:
+    print("นาย",name)
+else :
+    print("เด็กชาย",name)
+
